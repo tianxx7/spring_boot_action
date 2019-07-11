23df89d98f58592bce443257ef41ea14a9eea6b3
tian_xx <1729549607@qq.com>
2019-07-11 21:19:04 +0800

    测试activemq消息队列

---
+++ ch9_3_activemq/src/main/java/cn/txx/ch9_3_activemq/Ch93ActivemqApplication.java
@@ -0,0 +1,23 @@
+package cn.txx.ch9_3_activemq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.core.JmsTemplate;
+
+@SpringBootApplication
+public class Ch93ActivemqApplication implements CommandLineRunner {
+	@Autowired
+	JmsTemplate jmsTemplate; //2
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch93ActivemqApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        jmsTemplate.send("my-destination", new Msg()); //3
+
+    }
+}
